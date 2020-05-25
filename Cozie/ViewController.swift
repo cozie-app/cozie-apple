0a6d1c3ff954c92bf7e428adf53b17f428edd137
Federico Tartarini <federicotartarini@gmail.com>
2020-05-25 14:54:54 +0800

    Initial Commit

---
+++ Cozie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cozie
+//
+//  Created by Federico Tartarini on 25/5/20.
+//  Copyright © 2020 Federico Tartarini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
