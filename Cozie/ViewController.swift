1cc9189797c1aafc01be370457b872458dbd2656
Federico Tartarini <federicotartarini@gmail.com>
2020-05-26 17:13:57 +0800

    printing the results of the survey and added a bottom navigation bar

--- Cozie/ViewController.swift
+++ Cozie/ViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 import ResearchKit
 
 class ViewController: UIViewController, ORKTaskViewControllerDelegate {
+    
     func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
         //Handle results with taskViewController.result
         taskViewController.dismiss(animated: true, completion: nil)
+        
+        print("Start printing results ..................")
+        
+        if let results = taskViewController.result.results as? [ORKStepResult] {
+            print("Results: \(results)")
+            print("End results ........")
+            for stepResult: ORKStepResult in results {
+
+                for result in stepResult.results as! [ORKResult] {
+
+                    if let questionResult = result as? ORKQuestionResult {
+                        print("\(questionResult.identifier), \(questionResult.answer)")
+                    }
+                    if let tappingResult = result as? ORKTappingIntervalResult {
+                        print("\(tappingResult.identifier), \(tappingResult.samples), \(NSCoder.string(for: tappingResult.buttonRect1)) \(NSCoder.string(for: tappingResult.buttonRect1)))")
+                    }
+                    if let toneAudiometryResult = result as? ORKToneAudiometryResult {
+                        print("\(toneAudiometryResult.identifier), \(toneAudiometryResult.samples)")
+                    }
+                    if let spatialSpanResult = result as? ORKSpatialSpanMemoryResult {
+                        print("Score \(spatialSpanResult.score) Number of games \(spatialSpanResult.numberOfGames) Number of failuers \(spatialSpanResult.numberOfFailures)")
+                    }
+                    else{
+                        print("No printable results.")
+                    }
+
+                }
+            }
+
+        }
     }
     
     @IBAction func ConsentButton(_ sender: Any) {
@@ -29,9 +60,14 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
+class SettingsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
