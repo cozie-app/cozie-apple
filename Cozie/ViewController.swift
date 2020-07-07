396668cacdf9051747399499b3cd58aba2e82e37
Federico Tartarini <federicotartarini@gmail.com>
2020-07-07 15:57:00 +0800

    * added a collection view to display tasks to user

--- Cozie/ViewController.swift
+++ Cozie/ViewController.swift
@@ -22,7 +22,50 @@ struct AnswerResearchKit: Codable {
 }
 
 
-class ViewController: UIViewController, ORKTaskViewControllerDelegate {
+class ViewController: UIViewController, ORKTaskViewControllerDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    let tasksToCompleteLabels = ["Consent", "Eligibility", "Survey", "On-boarding"]
+    let tasksImages = [UIImage(named: "consentForm"), UIImage(named: "eligibility"), UIImage(named: "survey"), UIImage(named: "onBoarding")]
+    let tasksToPerform = [TaskConsent, TaskEligibility, TaskSurvey, TaskOnBoarding]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        tasksToCompleteLabels.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let taskViewController = ORKTaskViewController(task: tasksToPerform[indexPath.row], taskRun: nil)
+        taskViewController.delegate = self
+        present(taskViewController, animated: true, completion: nil)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+
+        cell.TaskImage.image = tasksImages[indexPath.row]
+        cell.TaskLabel.text = tasksToCompleteLabels[indexPath.row]
+
+        // todo change checkmark if the user completed the task or alternatively hide the view or move it to the bottom
+
+        // This creates the shadows and modifies the cards a little bit
+        cell.contentView.backgroundColor = UIColor.white
+        cell.contentView.layer.cornerRadius = 15.0
+        cell.contentView.layer.borderWidth = 1.0
+        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+        cell.contentView.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.gray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        cell.layer.shadowRadius = 4.0
+        cell.layer.shadowOpacity = 1.0
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+
+        return cell
+    }
 
     func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
         //Handle results with taskViewController.result
@@ -33,6 +76,7 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate {
                 Timestamp: GetDateTimeISOString(),
                 participantID: "test999", responses: [:])
 
+        // todo the code below is not working only for consent task
         if let results = taskViewController.result.results as? [ORKStepResult] {
             for stepResult: ORKStepResult in results {
                 for result in stepResult.results! {
@@ -44,8 +88,8 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate {
                 }
             }
         }
-        
-        // todo send answer to database
+
+        SendDataDatabase(answer: answer)
 
 //
 //        if let results = taskViewController.result.results as? [ORKStepResult] {
@@ -81,35 +125,44 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate {
 //        }
     }
 
-    @IBAction func ConsentButton(_ sender: Any) {
-        let taskViewController = ORKTaskViewController(task: TaskConsent, taskRun: nil)
-        taskViewController.delegate = self
-        present(taskViewController, animated: true, completion: nil)
+    private func SendDataDatabase(answer: AnswerResearchKit) {
+
+        var messages = [AnswerResearchKit]()
+
+        // check if answers are stored locally in UserDefaults, the key is answers
+        if let data = UserDefaults.standard.value(forKey: "AnswerResearchKit") as? Data {
+
+            // decode the messages stored in the local memory and convert them back to structures
+            let storedMessages = try? PropertyListDecoder().decode(Array<AnswerResearchKit>.self, from: data)
+
+            // add the last completed survey
+            messages = storedMessages!
+        }
+
+        messages += [answer]
+
+        var indexMessagesToDelete = [Int]()
+
+        for (index, message) in messages.enumerated() {
+            do {
+                let postMessage = try JSONEncoder().encode(message)
+                let statusCodeHTTP = PostRequest(message: postMessage)
+                if (statusCodeHTTP == 200) {
+                    indexMessagesToDelete.append(index)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+
+        // delete the messages that have been sent
+        for index in indexMessagesToDelete.reversed() {
+            messages.remove(at: index)
+        }
+
+        // save the messages to local storage so I replace what was previously there
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(messages), forKey: "AnswerResearchKit")
     }
-
-    @IBAction func SurveyButton(_ sender: Any) {
-        let taskViewController = ORKTaskViewController(task: TaskSurvey, taskRun: nil)
-        taskViewController.delegate = self
-        present(taskViewController, animated: true, completion: nil)
-    }
-
-    @IBAction func EligibilityButton(_ sender: Any) {
-        let taskViewController = ORKTaskViewController(task: TaskEligibility, taskRun: nil)
-        taskViewController.delegate = self
-        present(taskViewController, animated: true, completion: nil)
-    }
-
-    @IBAction func ParticipantInfo(_ sender: Any) {
-        let taskViewController = ORKTaskViewController(task: TaskOnBoarding, taskRun: nil)
-        taskViewController.delegate = self
-        present(taskViewController, animated: true, completion: nil)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
 }
 
 class SettingsController: UIViewController {
