88b1e8ef95883bc10cef3438af09fd3449bbcdfe
Deep <deep.g@squareinfosotf.com>
2021-12-17 15:44:48 +0530

    dailyParticipation, notificationFeq, participationDays popup design added

---
+++ Cozie/PopUps/DailyParticipation.swift
@@ -0,0 +1,24 @@
+//
+//  DailyParticipation.swift
+//  Cozie
+//
+//  Created by Square Infosoft on 17/12/21.
+//  Copyright © 2021 Federico Tartarini. All rights reserved.
+//
+
+import UIKit
+
+class DailyParticipation: UIViewController {
+
+    @IBOutlet weak var fromView: UIView!
+    @IBOutlet weak var toView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func setup() {
+        
+    }
+    
+}
