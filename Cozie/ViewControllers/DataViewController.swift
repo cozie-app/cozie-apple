41802b83f3203335682ad2c936a1547f0614d155
Deep <deep.g@squareinfosoft.com>
2022-01-21 10:14:52 +0530

    add: graph data page UI changes

--- Cozie/ViewControllers/DataViewController.swift
+++ Cozie/ViewControllers/DataViewController.swift
@@ -15,8 +15,8 @@ class DataViewController: UIViewController, ChartViewDelegate{
     @IBOutlet weak var graphView1: UIView!
     @IBOutlet weak var graphView2: UIView!
     @IBOutlet weak var dataDownloadView: UIView!
-    var stringVal1 = ["Indoor", "Outdoor"]
-    var stringVal2 = ["5 pax", "1-4 pax", "0 pax"]
+    var stringVal1 = ["02/12/2021", "03/12/2021", "04/12/2021"]
+    let stringVal2 = ["Valid Responses", "Goal"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,26 +44,26 @@ class DataViewController: UIViewController, ChartViewDelegate{
         barChartView2.bottomAnchor.constraint(equalTo: self.graphView2.layoutMarginsGuide.bottomAnchor).isActive = true
         
         let yValues1:[BarChartDataEntry] = [
-            BarChartDataEntry(x: 0, y: 55),
-            BarChartDataEntry(x: 1, y: 30),
+            BarChartDataEntry(x: 0, y: 9),
+            BarChartDataEntry(x: 1, y: 6),
+            BarChartDataEntry(x: 2, y: 12)
         ]
         
         let yValues2:[BarChartDataEntry] = [
-            BarChartDataEntry(x: 0, y: 50),
-            BarChartDataEntry(x: 1, y: 35),
-            BarChartDataEntry(x: 2, y: 5)
+            BarChartDataEntry(x: 0, y: 55),
+            BarChartDataEntry(x: 1, y: 80)
         ]
         
         self.setChartValue(values: yValues1,barChartView: barChartView1)
-        barChartView1.xAxis.labelCount = 2
+        barChartView1.xAxis.labelCount = self.stringVal1.count
         barChartView1.xAxis.valueFormatter = IndexAxisValueFormatter(values: self.stringVal1)
         self.setChartValue(values: yValues2, barChartView: barChartView2)
-        barChartView2.xAxis.labelCount = 3
+        barChartView2.xAxis.labelCount = self.stringVal2.count
         barChartView2.xAxis.valueFormatter = IndexAxisValueFormatter(values: self.stringVal2)
     }
     
-    private func barChart() -> HorizontalBarChartView{
-        let chartView = HorizontalBarChartView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.width))
+    private func barChart() -> BarChartView{
+        let chartView = BarChartView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.width))
         
         let yAxis = chartView.rightAxis
         yAxis.drawAxisLineEnabled = false
@@ -81,7 +81,7 @@ class DataViewController: UIViewController, ChartViewDelegate{
         return chartView
     }
     
-    private func setChartValue(values: [BarChartDataEntry], barChartView: HorizontalBarChartView){
+    private func setChartValue(values: [BarChartDataEntry], barChartView: BarChartView){
         
         let set = BarChartDataSet(entries: values,label: "No. of responses")
         set.colors = [.systemRed]
@@ -100,10 +100,10 @@ class DataViewController: UIViewController, ChartViewDelegate{
     
     @objc func onClickDataDownloadView(_ :UITapGestureRecognizer ){
         let alert = UIAlertController(title: "Download Data", message: "Are you sure you want to download your data? This might take a few moments", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Download", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Download", style: .default, handler: { _ in
+            print("download")
+        }))
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
-    
-    
 }
