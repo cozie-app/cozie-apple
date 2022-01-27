1bd137ef6748d1bfcc1020bfaf6daadd97e3bf11
Deep <deep.g@squareinfosoft.com>
2022-01-27 13:25:33 +0530

    add: weekly survey API

--- Cozie/PopUps/WeeklySurvey.swift
+++ Cozie/PopUps/WeeklySurvey.swift
@@ -14,8 +14,8 @@ class WeeklySurvey: BasePopupVC {
     @IBOutlet weak var buttonSubmit: UIButton!
     var questions = ["On which days did you work from home this week?", "Are you experiencing any of the following symptoms? (Select all that apply)","How much fatigue are you currently experiencing?","Please indicate your satisfaction levels with the overall indoor air quality in your office.","How much fatigue have you been experiencing throught the week?"]
     var options:[[String]] = [["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],["None Hoarse/dry throat", "Hoarse/dry throat", "Irritation of the eyes", "Dry eyes", "Dry skin", "Flushed facial skin", "Others, please indicate"],["Not at all", "Slightly", "Moderately", "Very", "Extremely"],["Extremely dissatisfied", "Moderately dissatisfied", "Slightly dissatisfied", "Neither satisfied or dissatisfied", "Slightly satisfied", "Moderately satisfied", "Extremely satisfied"],["Not at all", "Slightly", "Moderately", "Very", "Extremely"]]
-    var answers = [-1,-1,-1,-1,-1]
-    var buttons:[Set<UIButton>] = [[],[],[],[],[]]
+    var answers: [Int:Int] = [:]
+    var buttons: [Int:[UIButton]] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,12 +28,21 @@ class WeeklySurvey: BasePopupVC {
     }
     
     @IBAction func onClickSubmit(_ sender: UIButton) {
-        if self.answers.contains(-1) {
+        if self.answers.count != self.questions.count {
             let alert = UIAlertController(title: "Question remaining", message: "Make sure to answer every question in survey", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
             self.present(alert, animated: true, completion: nil)
         } else {
-            //TODO: API call
+            var tmpResponses: [String: String] = [:]
+            for (index,question) in self.questions.enumerated() {
+                tmpResponses[question] = options[index][answers[index] ?? 0]
+            }
+            do {
+                let postMessage = try JSONEncoder().encode(APIFormate(locationTimestamp: GetDateTimeISOString(), startTimestamp: GetDateTimeISOString(), endTimestamp: GetDateTimeISOString(), participantID: UserDefaults.shared.getValue(for: UserDefaults.UserDefaultKeys.participantID.rawValue) as? String ?? "", responses: tmpResponses))
+                PostRequest(message: postMessage)
+            } catch let error {
+                print(error.localizedDescription)
+            }
             NavigationManager.dismiss(self)
         }
     }
@@ -80,7 +89,7 @@ extension WeeklySurvey: UITableViewDelegate, UITableViewDataSource {
         let cell = tableQuestions.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! QuestionCell
         cell.labelQuestion.text = options[indexPath.section][indexPath.row]
         cell.delegate = self
-        self.buttons[indexPath.section].insert(cell.button)
+        self.buttons[indexPath.section]?.append(cell.button)
         cell.button.isSelected = self.answers[indexPath.section] == indexPath.row
         cell.button.backgroundColor = cell.button.isSelected ? .lightGray : .systemBackground
         return cell
@@ -90,7 +99,7 @@ extension WeeklySurvey: UITableViewDelegate, UITableViewDataSource {
 extension WeeklySurvey: selectAnswerDelegate {
     func onClickOption(cell: QuestionCell) {
         if let indexPath = tableQuestions.indexPath(for: cell) {
-            buttons[indexPath.section].forEach({$0.backgroundColor = .systemBackground})
+            buttons[indexPath.section]?.forEach({$0.backgroundColor = .systemBackground})
             cell.button.isSelected = true
             cell.button.backgroundColor = .lightGray
             self.answers[indexPath.section] = indexPath.row
