7adcfe96152744de168e0a799b7f9ece6f534544
Mario Frei <mario.frei@gmx.net>
2022-05-19 16:46:26 +0800

    Change OneSignal app ID

--- Cozie/AppDelegate.swift
+++ Cozie/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Code needed for OneSignal to work properly
         OneSignal.initWithLaunchOptions(launchOptions,
-          appId: "4d2b287e-603c-47db-a2b8-80b2a5d93473",
+          appId: "17d346bf-bfe5-4422-be96-2a8e4ae4cc3d",
           handleNotificationAction: nil,
           settings: onesignalInitSettings)
 
