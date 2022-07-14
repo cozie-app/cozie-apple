1756939e76fb249fe59092d26b6ad3f6217c64bf
sbbfti <federicotartarini@gmail.com>
2022-07-14 15:19:06 +0800

    feat: cleaning lambda cozie write

--- Lambda/cozie-apple-app-write-influx.py
+++ Lambda/cozie-apple-app-write-influx.py
@@ -3,17 +3,11 @@
 # Author: Mario Frei, 2022
 # Status: Under development
 # Project: Cozie
-# Experiemnt: Osk
-
-# To Do:
-#  - clean up field names (make json payload and database fields equal)
-#  - Add error handling
 
 import os
 import json
 from influxdb import InfluxDBClient, DataFrameClient
 import time
-import logging
 from pprint import pprint
 
 # influx authentication
@@ -21,9 +15,7 @@ user = os.environ['DB_USER']
 password = os.environ['DB_PASSWORD']
 host = os.environ['DB_HOST']
 port = os.environ['DB_PORT']
-db = os.environ['DB_NAME']
-measurement = os.environ['DB_MEASUREMENT']  # sql-table equivalent in InfluxDB
-
+db = "cozie-apple"
 
 def lambda_handler(event, context):
 
@@ -41,14 +33,13 @@ def lambda_handler(event, context):
 
     # Read payload
     payload = json.loads(event['body'])
-
     pprint(payload)
 
     # Check for minimal presence of required fields/tags
-    required_keys = ['participantID', 'endTimestamp']
+    required_keys = ['id_participant', 'timestamp_end']
     for key_required in required_keys:
         if key_required not in payload:
-            print(f"Either of the following keys was not in the payload")
+            print(f"Either the {required_keys} were not in the payload")
             return {
                 "statusCode": 500,
                 "headers": {
@@ -60,72 +51,57 @@ def lambda_handler(event, context):
     # Check for fields
     fields = {}
     json_body = []
-    tags = ['participantID', 'deviceUUID', 'user_id', 'experiment_id', 'endTimestamp']
+    tags = ['id_participant', 'id_device', 'id_experiment', 'timestamp_end']
 
     for key in payload.keys():
         # Check if the key value is an integer or a float
-        print("---------------------")
-        print("Key: ", key)
+        print("# Key: ", key)
         if key in tags:
-            print('tag')
+            # print('tag')
+            pass
         elif key == 'responses':
-            print("responses")
+            # print("responses")
             for key_r in payload[key].keys():
                 fields[key_r] = payload[key][key_r]
-        elif key == 'heartRate' or key == 'ts_' or key == 'noiseWatch':  # in key:
-            print("timeseries")
+        elif key == 'heart_rate' or key == 'ts_' or key == 'sound_pressure':  # in key:
             for key_ts in payload[key].keys():
-                timestamp_ts = key_ts  # int(datetime.strptime(key_ts,
-                # "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()*1000*1000*1000)   # Can be removed
-                print("key_ts: ", key_ts)
-                print("timestamp_ts: ", timestamp_ts)
-                print("timestamp_test", int(time.time()))
-                # timestamp_ts = 'xxx' # for debugging
-                fields_ts = {}
-                if (key == 'heartRate'):
-                    fields_ts[key] = float(payload[key][
-                        key_ts])  # This is necesssary because heartRate has been
-                    # stored as a float before can be removed when data is stored in a
-                    # new table
-                else:
-                    fields_ts[key] = payload[key][key_ts]
+                timestamp_ts = key_ts
+                print("key_ts: ", key_ts, "timestamp_ts: ", timestamp_ts, "timestamp_test", int(time.time()))
                 json_body.append({
                     'time': timestamp_ts,  # XXX
                     'measurement': measurement,
                     'tags': {
-                        'user_id': payload['participantID'],
-                        'device_uuid': payload['deviceUUID']
+                        'id_participant': payload['id_participant'],
+                        'id_device': payload['id_device']
                         },
-                    'fields': fields_ts
+                    'fields': {key: payload[key][key_ts]}
                     })
         else:
-            print("other")
-            if isinstance(payload[key], int):
-                payload[key] = float(payload[
-                    key])  # This conversion is needed because there is already
-                # float-type data in the database for some of the fields.
+            # print("other")
+            # if isinstance(payload[key], int):
+            #     payload[key] = float(payload[key])  # This conversion is needed because there is already
+            #     # float-type data in the database for some of the fields.
             fields[key] = payload[key]
         print("---------------------")
 
     # Convert timestamp from string to
-    # timestamp = int(datetime.strptime(payload['endTimestamp'],
+    # timestamp = int(datetime.strptime(payload['timestamp_end'],
     # "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()*1000*1000)  # Can be removed
-    timestamp = payload['endTimestamp']  # for debugging
-    print("endTimestamp: ", payload['endTimestamp'])
+    timestamp = payload['timestamp_end']  # for debugging
+    print("timestamp_end: ", payload['timestamp_end'])
     json_body.append({
         'time': timestamp,
         'measurement': measurement,
         'tags': {
-            'user_id': payload['participantID'],
-            'device_uuid': payload['deviceUUID']
+            'id_participant': payload['id_participant'],
+            'id_device': payload['id_device']
             },
         'fields': fields
         })
 
     print("##########################################################")
     print("json_body:")
-    print(type(json_body))
-    # print(json_body)
+    # print(type(json_body))
     print(json.dumps(json_body, indent=4))
     print("##########################################################")
     feedback = client.write_points(json_body)  # write to InfluxDB
