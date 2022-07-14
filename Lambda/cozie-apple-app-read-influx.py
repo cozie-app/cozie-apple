f1a6ac95bea3a02238e44de7ae586331380040e4
sbbfti <federicotartarini@gmail.com>
2022-07-14 17:27:43 +0800

    feat: modified lambda function to read

--- Lambda/cozie-apple-app-read-influx.py
+++ Lambda/cozie-apple-app-read-influx.py
@@ -8,38 +8,37 @@
 import json
 import datetime
 import pandas as pd
-import numpy as np
 from influxdb import InfluxDBClient
 import os
+
+# Influx authentication
+user = os.environ["DB_USER"]
+password = os.environ["DB_PASSWORD"]
+host = os.environ["DB_HOST"]
+port = os.environ["DB_PORT"]
+db = "cozie-apple"
 
 
 def lambda_handler(event, context):
 
-    # Influx authentication
-    user = os.environ['DB_USER']
-    password = os.environ['DB_PASSWORD']
-    host = os.environ['DB_HOST']
-    port = os.environ['DB_PORT']
-    db = os.environ['DB_NAME']
-    measurement = os.environ['DB_MEASUREMENT']  # sql-table equivalent in InfluxDB
-    # measurement = "cozieApple_debug" # For debugging
+    measurement = event["queryStringParameters"]["id_experiment"]
 
     # Check if there are any parameters, if not 400 error
-    if (len(event["queryStringParameters"]) == 0):
+    if len(event["queryStringParameters"]) == 0:
         return {
             "statusCode": 400,
             "body": json.dumps(
                 "query string parameter not defined. Please indicate the experiment-id "
                 "in the url string."
-                ),
-            }
+            ),
+        }
 
-    # Check if experiment-id is provided, if not send 400 error
-    if "user_id" not in event["queryStringParameters"]:
+    # Check if experiment_id or participant_id is provided, if not send 400 error
+    if ("id_participant" not in event["queryStringParameters"]) or ("id_experiment" not in event["queryStringParameters"]):
         return {
             "statusCode": 400,
-            "body": json.dumps("participant-id not in url-string"),
-            }
+            "body": json.dumps("participant_id or experiment_id not in url-string"),
+        }
 
     # Select the number of weeks to query data: Default is 2 weeks
     if "weeks" in event["queryStringParameters"]:
@@ -53,42 +52,87 @@ def lambda_handler(event, context):
 
     # Limit start time of data to be retrieved
     # This hack is needed because some fault data was inserted on 29.03.2022. All queries including data from data they return empty. The exact reason is unkown.
-    influx_time_horizon = datetime.datetime.strptime("30.03.2022, 00:00",
-        "%d.%m.%Y, %H:%M")
+    influx_time_horizon = datetime.datetime.strptime(
+        "30.03.2022, 00:00", "%d.%m.%Y, %H:%M"
+    )
     if time_start < influx_time_horizon:
         time_start = influx_time_horizon
 
     from_time_str = time_start.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    # query the entire database and return in pandas compatible format
-    if "user_id" in event["queryStringParameters"]:
+    # section of the code run if query comes from the cozie apple app
+    if "id_participant" in event["queryStringParameters"]:
         # Influx client
-        client = InfluxDBClient(host, port, user, password, db, ssl=True, verify_ssl=True)
+        client = InfluxDBClient(
+            host, port, user, password, db, ssl=True, verify_ssl=True
+        )
 
-        query_influx = "SELECT * FROM testingDB.autogen.{} WHERE time > '{}' AND user_id='{}'".format(
-            measurement, from_time_str,
-            event["queryStringParameters"]["user_id"])  # Original query
-
-        # Restrict query so that the resulting dataframe does not exceed the 6MB limit of AWS Lambda
-
-        # query_influx = "SELECT \"user_id\", \"device_uuid\",\"voteLog\", \"end\",
-        # \"clo\", \"location-place\", \"location-in-out\", \"tc-preference\", \"met\",
-        # \"any-change\", \"last-60min\", \"alone-group\", \"surroundings-infection\"
-        # FROM testingDB.autogen.cozieApple WHERE time > '{}' AND user_id='{}'".format(
-        # from_time_str, event["queryStringParameters"]["user_id"])
-
+        # todo query only the columns which do not have timeseries data
         query_influx = """SELECT 
-                          \"user_id\", \"device_uuid\",\"voteLog\", \"end\", \"clo\", \"location-place\", \"location-in-out\", \"tc-preference\", \"met\", \"any-change\", \"last-60min\", \"alone-group\", \"surroundings-infection\", 
-                          \"surroundings-infection\", \"within-5m\", \"cause-risk\", \"concerns\", 
-                          \"tc-preference\", \"light-preference\", \"sound-preference\", \"are-you\", \"location-place \", \"near-sensor?\", \"mood\", \"clo\", \"changed-loaction\", 
-                          \"alone-group\", \"activity\", \"distracting\", \"distractions\", \"activity\", \"more-privacy\", \"kind-distraction\", \"why-more-privacy\", \"what-privacy\", \"people-see\", \"activity\", \"surroundings-infection\", \"within-5m\", \"cause-risk\", \"concerns\", 
-                          \"last-60min\", \"lift-why\", \"stairs-why\", \"lift-con\", \"stairs-con\", \"working\", \"workstation\", \"adj-height\", \"current\", \"lift-why\", \"stairs-why\", \"lift-con\", 
-                          \"latitude\", \"longitude\",
-                          \"How much fatigue have you been experiencing throught the week?\", \"How much fatigue are you currently experiencing?\", \"On which days did you work from home this week?\", \"Please indicate your satisfaction levels with the overall indoor air quality in your office.\", \"Are you experiencing any of the following symptoms? (Select all that apply)\" 
-                          FROM testingDB.autogen.{} WHERE time > '{}' AND user_id='{}'""".format(
-            measurement, from_time_str, event["queryStringParameters"]["user_id"])
-        # \"locationTimestamp\", \"latitude\", \"longitude\", \"endTimestamp\", \"startTimestamp\",
-        # The following a hack upon request from the freelancers: \"latitude\", \"longitude\",
+                          \"id_participant\", 
+                          \"id_device\",
+                          \"vote_count\", 
+                          \"end\", 
+                          \"clo\", 
+                          \"location-place\", 
+                          \"location-in-out\", 
+                          \"tc-preference\", 
+                          \"met\", 
+                          \"any-change\", 
+                          \"last-60min\", 
+                          \"alone-group\", 
+                          \"surroundings-infection\",
+                          \"surroundings-infection\", 
+                          \"within-5m\", 
+                          \"cause-risk\", 
+                          \"concerns\",
+                          \"tc-preference\", 
+                          \"light-preference\", 
+                          \"sound-preference\", 
+                          \"are-you\", 
+                          \"location-place \", 
+                          \"near-sensor?\", 
+                          \"mood\", 
+                          \"clo\", 
+                          \"changed-loaction\",
+                          \"alone-group\", 
+                          \"activity\", 
+                          \"distracting\", 
+                          \"distractions\", 
+                          \"activity\", 
+                          \"more-privacy\", 
+                          \"kind-distraction\", 
+                          \"why-more-privacy\", 
+                          \"what-privacy\", 
+                          \"people-see\", 
+                          \"activity\", 
+                          \"surroundings-infection\", 
+                          \"within-5m\", 
+                          \"cause-risk\", 
+                          \"concerns\",
+                          \"last-60min\", 
+                          \"lift-why\", 
+                          \"stairs-why\", 
+                          \"lift-con\", 
+                          \"stairs-con\", 
+                          \"working\", 
+                          \"workstation\", 
+                          \"adj-height\", 
+                          \"current\", 
+                          \"lift-why\", 
+                          \"stairs-why\", 
+                          \"lift-con\",
+                          \"latitude\", 
+                          \"longitude\",
+                          \"How much fatigue have you been experiencing throught the week?\", 
+                          \"How much fatigue are you currently experiencing?\", 
+                          \"On which days did you work from home this week?\", 
+                          \"Please indicate your satisfaction levels with the overall indoor air quality in your office.\", 
+                          \"Are you experiencing any of the following symptoms? (Select all that apply)\" 
+                          FROM {}.autogen.{} WHERE time > '{}' AND id_participant='{}'""".format(
+            db, measurement, from_time_str, event["queryStringParameters"]["id_participant"]
+        )
+
         print(query_influx)
         result = client.query(query_influx)
         print("----------------------")
@@ -101,29 +145,31 @@ def lambda_handler(event, context):
         # In order to keep the example on the website working and the app version from the freelancers working there is some adjustment needed to the datetime-index
         # Ideally, the website and app are adapted in order to remove the following lines. (Using the DataFrameClient instead of the InfluxDBClient might resolve this issue)
         df = pd.DataFrame.from_dict(result["cozieApple"])
-        df['time'] = pd.to_datetime(df['time'])
-        df['time'] = df['time'].dt.tz_localize(None)
-        df.index = df['time']
-        df = df.drop(['time'], axis=1)
+        df["time"] = pd.to_datetime(df["time"])
+        df["time"] = df["time"].dt.tz_localize(None)
+        df.index = df["time"]
+        df = df.drop(["time"], axis=1)
 
         # In order to keep the Cozie app from freezinig for more than 30 seconds the last_sync_timesetamp needs to be returned
-        query_influx2 = """SELECT "heartRate" FROM "testingDB"."autogen"."{}" WHERE time > '{}' AND user_id='{}' ORDER BY "time" DESC LIMIT 1 """.format(
-            measurement, from_time_str, event["queryStringParameters"]["user_id"])
+        query_influx2 = """SELECT "heartRate" FROM "{}"."autogen"."{}" WHERE time > '{}' AND id_participant='{}' ORDER BY "time" DESC LIMIT 1 """.format(
+            db, measurement, from_time_str, event["queryStringParameters"]["id_participant"]
+        )
         result2 = client.query(query_influx2)
         print("----------------------")
         print("result2")
         print(result2)
         print("----------------------")
         for item in result2["cozieApple"]:
-            last_sync_timestamp = datetime.datetime.strptime(item["time"],
-                '%Y-%m-%dT%H:%M:%S.%fZ').timestamp()
+            last_sync_timestamp = datetime.datetime.strptime(
+                item["time"], "%Y-%m-%dT%H:%M:%S.%fZ"
+            ).timestamp()
 
     # no data for that query were available
     except KeyError:
         df = pd.DataFrame()
 
     field1 = event["queryStringParameters"]
-    field1['last_sync_timestamp'] = last_sync_timestamp
+    field1["last_sync_timestamp"] = last_sync_timestamp
 
     df_json = df.to_json(orient="index")
     df_dict = json.loads(df_json)
@@ -135,7 +181,7 @@ def lambda_handler(event, context):
     json_body = [
         field1,
         {"data": df_dict},
-        ]
+    ]
     print("------------------------")
     print("json_body:")
     print(json_body)
--- Lambda/cozie-apple-app-write-influx.py
+++ Lambda/cozie-apple-app-write-influx.py
@@ -11,11 +11,12 @@ import time
 from pprint import pprint
 
 # influx authentication
-user = os.environ['DB_USER']
-password = os.environ['DB_PASSWORD']
-host = os.environ['DB_HOST']
-port = os.environ['DB_PORT']
+user = os.environ["DB_USER"]
+password = os.environ["DB_PASSWORD"]
+host = os.environ["DB_HOST"]
+port = os.environ["DB_PORT"]
 db = "cozie-apple"
+
 
 def lambda_handler(event, context):
 
@@ -32,26 +33,24 @@ def lambda_handler(event, context):
     client = InfluxDBClient(host, port, user, password, db, ssl=True, verify_ssl=True)
 
     # Read payload
-    payload = json.loads(event['body'])
+    payload = json.loads(event["body"])
     pprint(payload)
 
     # Check for minimal presence of required fields/tags
-    required_keys = ['id_participant', 'timestamp_end']
+    required_keys = ["id_participant", "timestamp_end"]
     for key_required in required_keys:
         if key_required not in payload:
             print(f"Either the {required_keys} were not in the payload")
             return {
                 "statusCode": 500,
-                "headers": {
-                    "Content-Type": "application/json"
-                    },
-                "body": "Error: Required fields or tags ar missing"
-                }
+                "headers": {"Content-Type": "application/json"},
+                "body": "Error: Required fields or tags ar missing",
+            }
 
     # Check for fields
     fields = {}
     json_body = []
-    tags = ['id_participant', 'id_device', 'id_experiment', 'timestamp_end']
+    tags = ["id_participant", "id_device", "id_experiment", "timestamp_end"]
 
     for key in payload.keys():
         # Check if the key value is an integer or a float
@@ -59,23 +58,32 @@ def lambda_handler(event, context):
         if key in tags:
             # print('tag')
             pass
-        elif key == 'responses':
+        elif key == "responses":
             # print("responses")
             for key_r in payload[key].keys():
                 fields[key_r] = payload[key][key_r]
-        elif key == 'heart_rate' or key == 'ts_' or key == 'sound_pressure':  # in key:
+        elif key == "heart_rate" or key == "ts_" or key == "sound_pressure":  # in key:
             for key_ts in payload[key].keys():
                 timestamp_ts = key_ts
-                print("key_ts: ", key_ts, "timestamp_ts: ", timestamp_ts, "timestamp_test", int(time.time()))
-                json_body.append({
-                    'time': timestamp_ts,  # XXX
-                    'measurement': payload['id_experiment'],
-                    'tags': {
-                        'id_participant': payload['id_participant'],
-                        'id_device': payload['id_device']
+                print(
+                    "key_ts: ",
+                    key_ts,
+                    "timestamp_ts: ",
+                    timestamp_ts,
+                    "timestamp_test",
+                    int(time.time()),
+                )
+                json_body.append(
+                    {
+                        "time": timestamp_ts,  # XXX
+                        "measurement": payload["id_experiment"],
+                        "tags": {
+                            "id_participant": payload["id_participant"],
+                            "id_device": payload["id_device"],
                         },
-                    'fields': {key: payload[key][key_ts]}
-                    })
+                        "fields": {key: payload[key][key_ts]},
+                    }
+                )
         else:
             # print("other")
             # if isinstance(payload[key], int):
@@ -87,17 +95,19 @@ def lambda_handler(event, context):
     # Convert timestamp from string to
     # timestamp = int(datetime.strptime(payload['timestamp_end'],
     # "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()*1000*1000)  # Can be removed
-    timestamp = payload['timestamp_end']  # for debugging
-    print("timestamp_end: ", payload['timestamp_end'])
-    json_body.append({
-        'time': timestamp,
-        'measurement': payload['id_experiment'],
-        'tags': {
-            'id_participant': payload['id_participant'],
-            'id_device': payload['id_device']
+    timestamp = payload["timestamp_end"]  # for debugging
+    print("timestamp_end: ", payload["timestamp_end"])
+    json_body.append(
+        {
+            "time": timestamp,
+            "measurement": payload["id_experiment"],
+            "tags": {
+                "id_participant": payload["id_participant"],
+                "id_device": payload["id_device"],
             },
-        'fields': fields
-        })
+            "fields": fields,
+        }
+    )
 
     print("##########################################################")
     print("json_body:")
@@ -110,8 +120,6 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-            },
-        "body": "Success"
-        }
+        "headers": {"Content-Type": "application/json"},
+        "body": "Success",
+    }
