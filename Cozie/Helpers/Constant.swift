9effcc07c2877a6ff7d94786db8a16798251a714
Mario Frei <mario.frei@gmx.net>
2022-08-15 22:13:11 +0800

    Change OneSignalAppID from "Grek/Herf key" to "Cozie-master-version key"

--- Cozie/Helpers/Constant.swift
+++ Cozie/Helpers/Constant.swift
@@ -26,4 +26,4 @@ let AWSReadURL = "https://wifmmwu7qe.execute-api.ap-southeast-1.amazonaws.com/de
 let AWSWriteAPIKey = "5LkKVBO1Zp2pbYBbnkQsb8njmf8sGB5zhMrYQmPd"
 let AWSReadAPIKey = "5LkKVBO1Zp2pbYBbnkQsb8njmf8sGB5zhMrYQmPd"
 
-let OneSignalAppID = "4d2b287e-603c-47db-a2b8-80b2a5d93473"
+let OneSignalAppID = "17d346bf-bfe5-4422-be96-2a8e4ae4cc3d"
