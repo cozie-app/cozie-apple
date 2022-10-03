7fc60b2dc77e2bcfa8016b8f756edc9aa31a2906
Mario Frei <mario.frei@gmx.net>
2022-10-03 17:26:57 +0800

    Check datatype of data to be inserted in the database & reject payloads from deprecated experiments.

--- Lambda/cozie-apple-app-write-influx.py
+++ Lambda/cozie-apple-app-write-influx.py
@@ -8,49 +8,67 @@ import os
 import json
 from influxdb import InfluxDBClient, DataFrameClient
 import time
+from datetime import datetime
 from pprint import pprint
+import requests
+from check_type import check_type
 
 # influx authentication
-user = os.environ["DB_USER"]
-password = os.environ["DB_PASSWORD"]
-host = os.environ["DB_HOST"]
-port = os.environ["DB_PORT"]
+user = os.environ['DB_USER']
+password = os.environ['DB_PASSWORD']
+host = os.environ['DB_HOST']
+port = os.environ['DB_PORT']
 db = "cozie-apple"
-
 
 def lambda_handler(event, context):
 
-    # # Forward request to node red
-    # print("Forward request to old Node-Red server:")
-    # node_red_url = "http://ec2-52-76-31-138.ap-southeast-1.compute.amazonaws.com:1880
-    # /cozie-apple"
-    # payload =  json.loads(event['body'])
-    # response = requests.post(node_red_url, json=payload)
-    # print(response.content)
-    # print("---------------------------")
+    # Forward request to node red
+    try:
+        print("Forward request to old Node-Red server:")
+        node_red_url = "http://ec2-52-76-31-138.ap-southeast-1.compute.amazonaws.com:1880/cozie-apple"
+        payload =  json.loads(event['body'])
+        response = requests.post(node_red_url, json=payload)
+        print(response.content)
+        print("---------------------------")
+    except:
+        print("Forward request to old Node-Red server failed.")    
 
     # Influx client
     client = InfluxDBClient(host, port, user, password, db, ssl=True, verify_ssl=True)
 
     # Read payload
-    payload = json.loads(event["body"])
+    payload = json.loads(event['body'])
     pprint(payload)
-
+    
+    # Reject old id_experiments
+    deprecated_experiments = ["shen",
+                              "senth",
+                              "nen",
+                              "nern",
+                              "resh"]
+    
+    if payload['id_experiment'] in deprecated_experiments:
+        print("id_experiment is", payload['id_experiment'], "That Cozie version is outdated and payloads are not parsed correctly. Hence all data is rejected in order to make the logs more readable.")
+    
     # Check for minimal presence of required fields/tags
-    required_keys = ["id_participant", "timestamp_end"]
+    required_keys = ['id_participant', 'timestamp_end', 'id_experiment']
     for key_required in required_keys:
         if key_required not in payload:
             print(f"Either the {required_keys} were not in the payload")
             return {
                 "statusCode": 500,
-                "headers": {"Content-Type": "application/json"},
-                "body": "Error: Required fields or tags ar missing",
-            }
-
+                "headers": {
+                    "Content-Type": "application/json"
+                    },
+                "body": "Error: Required fields or tags ar missing"
+                }
+    # Get timestmap from call of lambda function
+    timestamp_lambda = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    
     # Check for fields
     fields = {}
     json_body = []
-    tags = ["id_participant", "id_device", "id_experiment", "timestamp_end"]
+    tags = ['id_participant', 'id_device', 'id_experiment', 'timestamp_end']
 
     for key in payload.keys():
         # Check if the key value is an integer or a float
@@ -58,68 +76,61 @@ def lambda_handler(event, context):
         if key in tags:
             # print('tag')
             pass
-        elif key == "responses":
+        elif key == 'responses':
             # print("responses")
             for key_r in payload[key].keys():
                 fields[key_r] = payload[key][key_r]
-        elif key == "heart_rate" or key == "ts_" or key == "sound_pressure":  # in key:
+        elif key == 'heart_rate' or 'ts_' in key or key == 'sound_pressure':  # in key:
             for key_ts in payload[key].keys():
                 timestamp_ts = key_ts
-                print(
-                    "key_ts: ",
-                    key_ts,
-                    "timestamp_ts: ",
-                    timestamp_ts,
-                    "timestamp_test",
-                    int(time.time()),
-                )
-                json_body.append(
-                    {
-                        "time": timestamp_ts,  # XXX
-                        "measurement": payload["id_experiment"],
-                        "tags": {
-                            "id_participant": payload["id_participant"],
-                            "id_device": payload["id_device"],
+                print("key_ts: ", key_ts, "timestamp_ts: ", timestamp_ts, "timestamp_test", int(time.time()))
+
+                value_checked = check_type(key, payload[key][key_ts])
+                    
+                json_body.append({
+                    'time': timestamp_ts,  # XXX
+                    'measurement': payload['id_experiment'],
+                    'tags': {
+                        'id_participant': payload['id_participant'],
+                        'id_device': payload['id_device']
                         },
-                        "fields": {key: payload[key][key_ts]},
+                    'fields': {
+                        key: value_checked,
+                        'timestamp_lambda': timestamp_lambda       
                     }
-                )
+                    })
         else:
-            # print("other")
-            # if isinstance(payload[key], int):
-            #     payload[key] = float(payload[key])  # This conversion is needed because there is already
-            #     # float-type data in the database for some of the fields.
-            fields[key] = payload[key]
+            fields[key] = check_type(key, payload[key])
         print("---------------------")
 
     # Convert timestamp from string to
-    # timestamp = int(datetime.strptime(payload['timestamp_end'],
-    # "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()*1000*1000)  # Can be removed
-    timestamp = payload["timestamp_end"]  # for debugging
-    print("timestamp_end: ", payload["timestamp_end"])
-    json_body.append(
-        {
-            "time": timestamp,
-            "measurement": payload["id_experiment"],
-            "tags": {
-                "id_participant": payload["id_participant"],
-                "id_device": payload["id_device"],
+
+    timestamp = payload['timestamp_end']  # for debugging
+    print("timestamp_end: ", payload['timestamp_end'])
+    fields["timestamp_lambda"] = timestamp_lambda
+    json_body.append({
+        'time': timestamp,
+        'measurement': payload['id_experiment'],
+        'tags': {
+            'id_participant': payload['id_participant'],
+            'id_device': payload['id_device']
             },
-            "fields": fields,
-        }
-    )
+        'fields': fields
+        })
 
     print("##########################################################")
     print("json_body:")
     # print(type(json_body))
     print(json.dumps(json_body, indent=4))
     print("##########################################################")
-    feedback = client.write_points(json_body)  # write to InfluxDB
+    feedback = client.write_points(json_body, batch_size=5000)  # write to InfluxDB
     print("Client write: ", feedback)
     print("##########################################################")
 
     return {
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": "Success",
-    }
+        "headers": {
+            "Content-Type": "application/json"
+            },
+        "body": "Success"
+        }
