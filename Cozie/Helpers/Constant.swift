2b8f1029f9595f0e6dbe106b259935955ad01dca
Mario Frei <mario.frei@gmx.net>
2022-11-15 19:27:45 +0800

    Update app icon

--- Cozie/Helpers/Constant.swift
+++ Cozie/Helpers/Constant.swift
@@ -11,8 +11,8 @@ import UIKit
 let defaultFromTime = Calendar.current.date(bySettingHour: 8, minute: 0, second: 0, of: Date()) ?? Date()
 let defaultToTime = Calendar.current.date(bySettingHour: 18, minute: 0, second: 0, of: Date()) ?? Date()
 let defaultNotificationFrq = Calendar.current.date(bySettingHour: 1, minute: 0, second: 0, of: Date()) ?? Date()
-let defaultExperimentID = "AppleStore"
-let defaultParticipantID = "ExternalTester"
+let defaultExperimentID = "xesh"
+let defaultParticipantID = "xesh"
 
 let primaryColour = UIColor(named: "primaryColour")
 
@@ -26,4 +26,4 @@ let AWSReadURL = "https://wifmmwu7qe.execute-api.ap-southeast-1.amazonaws.com/de
 let AWSWriteAPIKey = "5LkKVBO1Zp2pbYBbnkQsb8njmf8sGB5zhMrYQmPd"
 let AWSReadAPIKey = "5LkKVBO1Zp2pbYBbnkQsb8njmf8sGB5zhMrYQmPd"
 
-let OneSignalAppID = "17d346bf-bfe5-4422-be96-2a8e4ae4cc3d"
+let OneSignalAppID = "677e9eb9-933c-48c0-8dac-d515a9f8325c"
