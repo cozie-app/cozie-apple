8bd7e537be281b9bb6adfee2c23d1b9e30317dc5
Mario Frei <mario.frei@gmx.net>
2022-12-17 03:02:02 +0800

    change random letter pool for participant id

--- Cozie/Helpers/Constant.swift
+++ Cozie/Helpers/Constant.swift
@@ -13,11 +13,10 @@ let defaultToTime = Calendar.current.date(bySettingHour: 18, minute: 0, second: 
 let defaultNotificationFrq = Calendar.current.date(bySettingHour: 1, minute: 0, second: 0, of: Date()) ?? Date()
 let defaultExperimentID = "AppleStore"
 
-
-let suffix_letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+// Generate randomized default participant id
+let suffix_letters = "ABCDEFGHKLMNPQRSTUVWXYZ23456789" //IJO01 are intentionally missing
 let suffix_length = 8
 let suffix = String((0..<suffix_length).map{ _ in suffix_letters.randomElement()! })
-
 let defaultParticipantID = "Participant_" + suffix
 
 let primaryColour = UIColor(named: "primaryColour")
