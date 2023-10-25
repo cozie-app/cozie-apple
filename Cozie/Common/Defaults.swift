a187f1722a6ce7145264e1acacdcf62372ac4453
Mario Frei <mario.frei@gmx.net>
2023-10-25 22:09:21 +0800

    change default phone survey link

--- Cozie/Common/Defaults.swift
+++ Cozie/Common/Defaults.swift
@@ -47,7 +47,7 @@ struct Defaults {
     static let watchSurveyLink: String = "https://raw.githubusercontent.com/cozie-app/cozie-apple/master/Watch%20Surveys/watch_survey_thermal_short.json"
     
     // The values below need to replace the values stored in AppLinks.swift
-    static let phoneSurveyLink: String = "https://docs.google.com/forms/d/e/1FAIpQLSchX6cIqgx7tupV_47o5sYVs5IvEBqhwTMGuRLCjGxqbh_gTA/viewform?usp=pp_url&entry.247006640=dev&entry.932499052=dev01"
+    static let phoneSurveyLink: String = "https://forms.gle/BGGJ8es2fz4uvmFc7"
     static let cozieWebsiteURL: String = "https://www.cozie-apple.app"
     static let cozieGithubURL: String = "https://github.com/cozie-app/cozie-apple"
     
