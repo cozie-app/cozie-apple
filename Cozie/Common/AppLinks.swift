a323f8f8278cb8dd39053c539e02951d3c8f4c41
Mario Frei <mario.frei@gmx.net>
2023-11-30 00:38:59 +0800

    change default phone survey url

--- Cozie/Common/AppLinks.swift
+++ Cozie/Common/AppLinks.swift
@@ -10,5 +10,5 @@ import Foundation
 enum AppLink: String {
     case documentation = "https://cozie-apple.app"
     case githubRepo = "https://github.com/cozie-app/cozie-apple"
-    case phoneSurvey = "https://docs.google.com/forms/d/e/1FAIpQLSchX6cIqgx7tupV_47o5sYVs5IvEBqhwTMGuRLCjGxqbh_gTA/viewform?usp=pp_url&entry.247006640=dev&entry.932499052=dev01"
+    case phoneSurvey = "https://forms.gle/BGGJ8es2fz4uvmFc7"
 }
